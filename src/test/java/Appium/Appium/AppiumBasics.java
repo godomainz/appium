230d04a1756c824183eb4c5e424dafea16afa402
Akila Mendis <akila@oneit.com.au>
2024-06-18 09:39:28 +0530

    26. Introduction to Appium Inspector to identify the elements on the Apps

--- src/test/java/Appium/Appium/AppiumBasics.java
+++ src/test/java/Appium/Appium/AppiumBasics.java
@@ -5,8 +5,10 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
@@ -31,8 +33,11 @@ public class AppiumBasics {
 		options.setApp("F:\\Eclipse Workspace\\Appium\\src\\test\\java\\resources\\ApiDemos-debug.apk");
 		
 		AndroidDriver driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
-		driver.quit();
 		
+		WebElement preferenceBtn = driver.findElement(AppiumBy.accessibilityId("Preference"));
+		preferenceBtn.click();
+		
+		driver.quit();
 		localService.stop();
 
 	}
