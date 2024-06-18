9b9c592f1229030140d18c4e43ec396ac83fc0b7
Akila Mendis <akila@oneit.com.au>
2024-06-18 10:03:25 +0530

    27. Creating Base Class with Util methods and extend it to Child Appium Tests

--- src/test/java/Appium/Appium/AppiumBasics.java
+++ src/test/java/Appium/Appium/AppiumBasics.java
@@ -1,45 +1,19 @@
 package Appium.Appium;
 
-import java.io.File;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.options.UiAutomator2Options;
-import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServiceBuilder;
 
-public class AppiumBasics {
+public class AppiumBasics extends BaseTest {
 	
 	@Test
-	private void AppiumTest() throws MalformedURLException, URISyntaxException {
-		
-		String appiumJS = "C:\\Users\\Akila\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js";
-		AppiumServiceBuilder service = new AppiumServiceBuilder();
-		service.withAppiumJS(new File(appiumJS));
-		service.withIPAddress("127.0.0.1");
-		service.usingPort(4723);
-		
-		AppiumDriverLocalService localService =  service.build();
-		localService.start();
-		
-		UiAutomator2Options options = new UiAutomator2Options();
-		options.setDeviceName("Pixel 4 API 30");
-		options.setApp("F:\\Eclipse Workspace\\Appium\\src\\test\\java\\resources\\ApiDemos-debug.apk");
-		
-		AndroidDriver driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
-		
+	private void wifiSettingsName() throws MalformedURLException, URISyntaxException {
 		WebElement preferenceBtn = driver.findElement(AppiumBy.accessibilityId("Preference"));
 		preferenceBtn.click();
-		
-		driver.quit();
-		localService.stop();
-
 	}
 
 }
---
+++ src/test/java/Appium/Appium/BaseTest.java
@@ -0,0 +1,44 @@
+package Appium.Appium;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+
+public class BaseTest {
+	
+	public AppiumDriverLocalService localService;
+	public AndroidDriver driver;
+	
+	@BeforeClass
+	public void configureAppium() throws MalformedURLException, URISyntaxException {
+		String appiumJS = "C:\\Users\\Akila\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js";
+		AppiumServiceBuilder service = new AppiumServiceBuilder();
+		service.withAppiumJS(new File(appiumJS));
+		service.withIPAddress("127.0.0.1");
+		service.usingPort(4723);
+		
+		localService =  service.build();
+		localService.start();
+		
+		UiAutomator2Options options = new UiAutomator2Options();
+		options.setDeviceName("Pixel 4 API 30");
+		options.setApp("F:\\Eclipse Workspace\\Appium\\src\\test\\java\\resources\\ApiDemos-debug.apk");
+		
+		driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+		localService.stop();
+
+	}
+}
