c7700fb1fb7b3b582fd270b52e34aaff92714c85
Akila Mendis <akila@oneit.com.au>
2024-06-18 11:18:13 +0530

    31. Introduction to Id, Xpath & Accessibility ID locators in App with an example

--- src/test/java/Appium/Appium/AppiumBasics.java
+++ src/test/java/Appium/Appium/AppiumBasics.java
@@ -3,6 +3,7 @@ package Appium.Appium;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
@@ -14,6 +15,15 @@ public class AppiumBasics extends BaseTest {
 	private void wifiSettingsName() throws MalformedURLException, URISyntaxException {
 		WebElement preferenceBtn = driver.findElement(AppiumBy.accessibilityId("Preference"));
 		preferenceBtn.click();
+		
+		WebElement preferenceDependenciesBtn = driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]"));
+		preferenceDependenciesBtn.click();
+		
+		WebElement wifiCheckBox = driver.findElement(By.id("android:id/checkbox"));
+		wifiCheckBox.click();
+		
+		WebElement wifiSettingsBtn = driver.findElement(By.xpath("//android.widget.ListView[@resource-id=\"android:id/list\"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout"));
+		wifiSettingsBtn.click();
 	}
 
 }
